591b91c7e3ad2557bf7a2410b3fcc78d5d8b8b42
mattprompt <mattprompt@gmail.com>
2021-08-05 22:53:37 -0700

    feat(DOCKER): Add dockerfile, patch, and .bat example scripts
    Patch file is used to add the ATTiny214 build target to avr-gcc.
    Bat example files are used to build and run docker image. Docker
    image is pushed to DockerHub at mattprompt/sub-etha-tinycomms:latest

---
+++ docker/build_docker.bat
@@ -0,0 +1,2 @@
+REM Example command to build docker image from docker file and tag
+docker build -t mattprompt/sub-etha-tinycomms:latest . -f sub-etha-tinycomms.dockerfile
---
+++ docker/run_docker.bat
@@ -0,0 +1,2 @@
+REM Example command to run the docker image and attach map a local directory to it
+docker run -it -v ./sub-etha-tinycomms:/sub-etha-tinycomms mattprompt/sub-etha-tinycomms:latest
